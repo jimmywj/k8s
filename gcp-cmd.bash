51a57cc6029179bbe51a27a6c7ca7a2cdb33febe
Jimmy Wong <jimmywj@gmail.com>
2018-03-20 15:27:28 +0800

    support for 1.9 and dynamic disk allocation

--- gcp-cmd.bash
+++ gcp-cmd.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcloud container clusters create clcluster --num-nodes=1 --cluster-version=1.8.5-gke.0
+gcloud container clusters create clcluster --num-nodes=3 --cluster-version=1.9.4-gke.1
 
 gcloud container get-server-config
